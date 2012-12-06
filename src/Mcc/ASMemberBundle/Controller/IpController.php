511fba3acd3267eb583468b705063f6420b508c9
Konrad Masalski <konradmas@gmail.com>
2012-12-06 21:33:57 +0100

    przeniesienie funkci

--- src/Mcc/ASMemberBundle/Controller/IpController.php
+++ src/Mcc/ASMemberBundle/Controller/IpController.php
@@ -455,4 +455,72 @@ class IpController extends Controller {
         
         return new Response(var_dump($this->search("wp.pl")));       		
     }
+    
+        public function download($url)
+    {
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Sitepoint Examples (thread 581410; http://www.sitepoint.com/forums/showthread.php?t=581410)');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+
+        set_time_limit(65);
+
+        curl_exec($ch);
+        $info = curl_getinfo($ch);
+
+        // Time spent downloading, I think
+        $time = $info['total_time']
+              - $info['namelookup_time']
+              - $info['connect_time']
+              - $info['pretransfer_time']
+              - $info['starttransfer_time']
+              - $info['redirect_time'];
+        
+        $statistics['size_download'] = $info['size_download'];
+        $statistics['time'] = $time;
+        $statistics['speedCurl'] = $info['speed_download'] * 8 / 1024 / 1024;
+        $statistics['speedObtained'] = $info['size_download'] * 8 / $time / 1024 / 1024;
+        
+        unset($ch);
+        return $statistics;
+    }
+    
+    public function downloadTestAction()
+    {
+        return new Response(var_dump($this->download("diety.wp.pl/regulamin_serwisu_wp.pdf")));
+    }
+    
+    public function saveFiles($links, $ip_adr)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $fileRepository = $em->getRepository('MccASsMember:File');
+        foreach ($links as $link) 
+        {
+            $statistics = $this->download($link);
+            
+            $file = $fileRepository->getFileByAddress($link);
+            if($file == null)
+            {
+                $file = new File();
+                $file->setAddress($link);
+                $file->setIpid($ip_adr);
+                $file->setSize($statistics['size_download']);
+                $em->persist($file);
+            }
+            
+            $history = new \Mcc\ASMemberBundle\Entity\History();
+            $history->setFileId();
+            $history->setSpeedCurl($statistics['speedCurl']);
+            $history->setSpeedObtained($statistics['speedObtained']);
+            $history->setTime($statistics['time']);
+            $history->setWhenchecked(time());
+            
+            $em->persist($history);
+            
+        }
+        $em->flush();
+    }
 }
